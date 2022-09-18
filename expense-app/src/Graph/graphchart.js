a7bdba06eceeb8595bca9e18fa2e5b85d5c3a42a
EugeneDao <hoangkhang0209@gmail.com>
2022-09-18 10:07:10 +0700

    graph add

---
+++ expense-app/src/Graph/graphchart.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GraphItem from './graphitems';
+function GraphChart() {
+    return (
+        <div style ={{backgroundColor:"E1CAE4"}}>
+            totalArray.map()
+        </div>
+    )
+};
+export default GraphChart;
