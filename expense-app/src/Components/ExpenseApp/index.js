0f4479af5a36ce16372720e0831daad3eee76d87
EugeneDao <hoangkhang0209@gmail.com>
2022-09-19 20:33:27 +0700

    changeParent

--- expense-app/src/Components/ExpenseApp/index.js
+++ expense-app/src/Components/ExpenseApp/index.js
@@ -3,11 +3,13 @@ import React from "react";
 import ExpenseList from "../ExpenseList";
 import Add from "../AddNewExpense";
 import "./style.css";
+import GraphChart from "../Graph/graphchart";
 
 function ExpenseApp(){
     return(
         <div>
             <Add />
+            <GraphChart />
             <ExpenseList />
         </div>
     )
