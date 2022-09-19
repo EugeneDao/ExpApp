32142cc91cf619d4c17b8e12bde1c4034328fd49
ttluan97ccnc <ttluan97ccnc@gmail.com>
2022-09-19 12:03:24 +0700

    Update-ExpenseItem-ExpenseList

--- expense-app/src/Components/ExpenseItem/index.js
+++ expense-app/src/Components/ExpenseItem/index.js
@@ -3,23 +3,42 @@ import React, {useState} from "react";
 import "./style.css";
 
 function ExpenseItem(props){
+
+    const convertMonth = (m) =>{
+        let monthName;
+
+        switch(m){
+            case 1: monthName="January"; break;
+            case 2: monthName="February"; break;
+            case 3: monthName="March"; break;
+            case 4: monthName="April"; break;
+            case 5: monthName="May"; break;
+            case 6: monthName="June"; break;
+            case 7: monthName="July"; break;
+            case 8: monthName="August"; break;
+            case 9: monthName="September"; break;
+            case 10: monthName="October"; break;
+            case 11: monthName="November"; break;
+            case 12: monthName="December"; break;
+        }
+
+        return monthName;
+    }
+
     return (
-        <div className="expense-item" onMouseEnter={handleMouseEnter}>
+        <div className="expense-item">
             
             <span className="date">
                 <h2 className="year">{props.year}</h2>
-                <h2 className="month">{props.month}</h2>
+                <h2 className="month">{convertMonth(props.month)}</h2>
                 <h1 className="day">{props.day}</h1>
             </span>
 
             <span className="title">{props.title}</span>
 
             <span className="amount">$ {props.amount}</span>
-
-            <i className="edit-button" onClick={handleOnClickEdit}>EDIT</i>
-            <i className="remove-button" onClick={handleOnClickRemove}>REMOVE</i>
         </div>
-    )
-}
+    );
+};
 
 export default ExpenseItem;
